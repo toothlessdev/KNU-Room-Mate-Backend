b41b5321e3ba87285dd390e9cb46625072d70d19
toothlessdev <toothless042@gmail.com>
2024-02-23 15:32:51 +0900

    config: PostgreSQL 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { RoommateModule } from './roommate/roommate.module';
+
+const RootEntities = [];
+
+const TypeOrmRootModule = TypeOrmModule.forRoot({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "knu-room-mate",
+    password: "knu-room-mate",
+    database: "knu-room-mate",
+    synchronize: true,
+
+    entities: RootEntities,
+});
 
 @Module({
-    imports: [],
+    imports: [TypeOrmRootModule, RoommateModule],
     controllers: [AppController],
     providers: [AppService],
 })
